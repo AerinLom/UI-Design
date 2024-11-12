16d242eb54283664d89d67bb51b26e5d94a49078
AerinLom <aerinlom1@gmail.com>
2024-11-12 15:14:19 +0200

    Add project files.

---
+++ UI Design/wwwroot/js/script.js
@@ -0,0 +1,37 @@
+﻿function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
+
+let currentSlideIndex = 0;
+const slides = document.querySelectorAll('.carousel-item');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlideIndex = (currentSlideIndex + 1) % slides.length;
+    showSlide(currentSlideIndex);
+}
+
+function prevSlide() {
+    currentSlideIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+    showSlide(currentSlideIndex);
+
+}
+
+// Initialize the first slide as active
+showSlide(currentSlideIndex);
+
