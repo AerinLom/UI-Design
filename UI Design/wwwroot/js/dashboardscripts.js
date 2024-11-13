30bf474f98da6359afe74e81e6d37e1180e1b57e
Matthew Palmer <mattpalmer950@gmail.com>
2024-11-13 16:28:08 +0200

    added dashboard page

---
+++ UI Design/wwwroot/js/dashboardscripts.js
@@ -0,0 +1,46 @@
+﻿window.onload = function () {
+    document.getElementById("sidebar").style.width = "0";
+};
+
+function createNewList() {
+    alert("Redirecting to list creation...");
+}
+
+function viewSavedLists() {
+    alert("Viewing saved lists...");
+}
+
+function viewPopularProducts() {
+    alert("Opening popular products...");
+}
+
+function login() {
+    alert("Redirecting to login page...");
+}
+
+function createAccount() {
+    alert("Redirecting to create account page...");
+}
+
+function manageAccount() {
+    alert("Redirecting to manage account page...");
+}
+
+function toggleDropdown() {
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    } else {
+        dropdownMenu.style.display = "block";
+    }
+}
+
+function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.querySelector(".toggle-sidebar").style.display = "inline-block";
+}
+
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.querySelector(".toggle-sidebar").style.display = "none";
+}
